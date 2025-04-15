b2c00011c73d0af88400f0c4851ef603b9be6822
Ethan <Sebasduran944@gmail.com>
2025-04-14 22:35:44 -0600

    Tres ejercios del curso y proyecto1

---
+++ programaStrings.py
@@ -0,0 +1,14 @@
+#Segundo programa que nos habla sobre los string y que todo lo que se encuentre en el striong sera tratado como texto
+print('100 + 50')
+print('Hola' + ' ' + 'Sebas')
+#Si se quiere agregar comillas a un texto de manera que estas se vean
+#Ejemplo: Hola me llamo "Sebastian"
+#Podemos utilizar la barra invertida \ para señalar a python que el siguiente caracter lo use como caracter especial
+print("Hola me llamo \"Sebastian\"")
+print("Esta es una linea\nY esta es una segunda linea")
+print("\t Esta es una linea tabulada")
+
+#Ejercicio 2 del curso
+print("A\tB\tC\nD\tE\tF\nG\tH\tI")
+#Ejercicio 3
+print("Brarra normal: /\nBarra invertida: \\")
